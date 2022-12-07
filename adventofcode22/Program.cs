7baaa689cd5abad3d16f87aa3dccece22c6353e2
Jan Roth <jan-roth1998@hotmail.com>
2022-12-07 18:36:59 +0100

    solution for day6

--- adventofcode22/Program.cs
+++ adventofcode22/Program.cs
@@ -3,6 +3,7 @@ using adventofcode22.puzzles.day2;
 using adventofcode22.puzzles.day3;
 using adventofcode22.puzzles.day4;
 using adventofcode22.puzzles.day5;
+using adventofcode22.puzzles.day6;
 
 namespace adventofcode22
 {
@@ -15,7 +16,8 @@ namespace adventofcode22
             //new Day2();
             //new Day3();
             //new Day4();
-            new Day5();
+            //new Day5();
+            new Day6();
         }
     }
 }
---
+++ adventofcode22/puzzles/day6/Day6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace adventofcode22.puzzles.day6
+{
+    public class Day6
+    {
+        public Day6()
+        {
+            var input = File.ReadAllText(@"puzzles\day6\input1.txt");
+            //part1(input);
+            part2(input);
+        }
+
+        public void part2(string input)
+        {
+            var noDuplicateIndex = 0;
+            for (int i = 0; i < input.Length - 14; i++)
+            {
+
+                if (Regex.IsMatch(input.Substring(i, 14), @"^(?:(.)(?!.*\1))*$"))
+                {
+                    noDuplicateIndex = i + 14;
+                    break;
+                }
+            }
+            Console.WriteLine(noDuplicateIndex);
+        }
+
+        public void part1(string input)
+        {
+            var noDuplicateIndex = 0;
+            for (int i = 0; i < input.Length - 4; i++)
+            {
+                if ((new char[] { input[i + 1], input[i + 2], input[i + 3] }).Contains(input[i])) { }
+                else if ((new char[] { input[i], input[i + 2], input[i + 3] }).Contains(input[i + 1])) { }
+                else if ((new char[] { input[i], input[i + 1], input[i + 3] }).Contains(input[i + 2])) { }
+                else
+                {
+                    noDuplicateIndex = i + 4;
+                    break;
+                }
+            }
+            Console.WriteLine(noDuplicateIndex);
+        }
+    }
+}
